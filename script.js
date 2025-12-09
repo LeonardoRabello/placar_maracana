c2a973607e50081ec36cc5aba701bba86b20441c
leonardo-rabello <16726190760@cefet-rj.br>
2025-12-08 23:51:31 -0300

    arquivo errado

--- script.js
+++
@@ -1,9 +0,0 @@
-function updatePlacar() {
-    document.getElementById("time-1").innerText = document.getElementById("time-1-label").value;
-
-    document.getElementById("time-2").innerText = document.getElementById("time-2-label").value;
-    
-    document.getElementById("score-1").innerText = document.getElementById("score-1-label").value;
-
-    document.getElementById("score-2").innerText = document.getElementById("score-2-label").value;
-}
